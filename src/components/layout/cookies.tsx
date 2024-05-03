3b65c4dc653520ffd1e2d20dd05d35628eb4fb16
Freitas-MA <freitas.ma.1993@gmail.com>
2024-05-03 20:17:10 +0100

    refactor: Update cookies notice component to use next/navigation instead of next/headers and next/cache

--- src/components/layout/cookies.tsx
+++ src/components/layout/cookies.tsx
@@ -1,42 +1,38 @@
+"use client";
 import Link from "next/link";
-import { cookies } from "next/headers";
-import { revalidatePath } from "next/cache";
-
+import { useRouter } from "next/navigation";
 interface CookiesNoticeProps {
 	className?: string; // Optional className prop
 }
 
 const CookiesNotice: React.FC<CookiesNoticeProps> = async ({ className }) => {
-	const hasAcceptedCookies = cookies().has("cookiesAccepted");
-
-	const handleAcceptCookies = async () => {
-		"use server";
-		console.log("Accepting cookies");
-		cookies().set("cookiesAccepted", "true");
-		revalidatePath("/");
+	const router = useRouter();
+	const handleAcceptCookies = async (e: React.FormEvent) => {
+		e.preventDefault();
+		const expirationDate = new Date();
+		expirationDate.setFullYear(expirationDate.getFullYear() + 1);
+		document.cookie = `cookiesAccepted=true; expires=${expirationDate.toUTCString()}; path=/`;
+		router.refresh();
 	};
 	return (
 		<>
-			{!hasAcceptedCookies && (
-				<div className={`${className}`}>
-					<p className="cookieHeading text-xl font-bold z-2">Cookies.</p>
-					<p className="cookieDescription flex flex-col text-base z-2">
-						This website uses cookies for a better user experience. By
-						continuing to use this site, you accept the use of cookies.{" "}
-						<Link className="text-blue-400" href="/privacy-policy">
-							Learn more
-						</Link>
-					</p>
-					<form action={handleAcceptCookies}>
-						<button
-							type="submit"
-							className="acceptButton w-32 mt-4 px-4 py-2 font-bold shadow-md bg-white hover:bg-slate-200 rounded transition duration-200 ease-in-out z-2"
-						>
-							Ok!
-						</button>
-					</form>
-				</div>
-			)}
+			<div className={`${className}`}>
+				<p className="cookieHeading text-xl font-bold z-2">Cookies.</p>
+				<p className="cookieDescription flex flex-col text-base z-2">
+					This website uses cookies for a better user experience. By continuing
+					to use this site, you accept the use of cookies.{" "}
+					<Link className="text-blue-400" href="/privacy-policy">
+						Learn more
+					</Link>
+				</p>
+				<button
+					type="submit"
+					className="acceptButton w-32 mt-4 px-4 py-2 font-bold shadow-md bg-white hover:bg-slate-200 rounded transition duration-200 ease-in-out z-2"
+					onClick={handleAcceptCookies}
+				>
+					Ok!
+				</button>
+			</div>
 		</>
 	);
 };
