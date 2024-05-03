7b922301bcab33e08b9bf2031d2cccca7f64739f
Freitas-MA <freitas.ma.1993@gmail.com>
2024-05-03 18:47:10 +0100

    refactor: Improve code readability and fix typos in NotFound component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,9 +5,9 @@ export default function NotFound() {
 	return (
 		<div className="flex flex-col w-full h-[85vh] justify-center items-center">
 			<h1>Oops!</h1>
-			<p>We couldn't find the page you're looking for.</p>
+			<p>We couldn&apos;t find the page you&apos;re looking for.</p>
 			<p>
-				But don't worry, you can still explore our delicious cocktail recipes!
+				But don&apos;t worry, you can still explore our delicious cocktail recipes!
 			</p>
 		</div>
 	);
