9ab27dc79f11153ba9c413bd573f26562fa6a4e8
Freitas-MA <freitas.ma.1993@gmail.com>
2024-05-03 16:24:16 +0100

    refactor: Remove unused code and improve code readability in NotFound component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import { redirect } from "next/navigation";
 
 export default function NotFound() {
-  const redirectHome = () => {
-    setTimeout(() => {
-      redirect("/");
-    }, 2000);
-  }
-  redirectHome();
+
 	return (
 		<div className="flex flex-col w-full h-[85vh] justify-center items-center">
 			<h1>Oops!</h1>
