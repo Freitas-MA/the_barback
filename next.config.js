69822894cb2fd5f1dd8a098894c9ad26f7c6b61c
Freitas-MA <freitas.ma.1993@gmail.com>
2024-05-03 18:23:32 +0100

    chore: Update next-pwa npm dependency to version 5.6.0

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
+const withPWA = require("next-pwa")({
+	dest: "public",
+});
 /** @type {import('next').NextConfig} */
-const nextConfig = {
 
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'oiwdggkucckvowjvkwel.supabase.co',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "oiwdggkucckvowjvkwel.supabase.co",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
+};
+
+module.exports = withPWA(nextConfig);
